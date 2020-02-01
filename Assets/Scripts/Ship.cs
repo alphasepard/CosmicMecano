404d66c631f331184731deb7a15cf44f5986a5c0
Benjamin Fraquet <benjamin.fraquet@gmail.com>
2020-02-01 13:36:36 +0000

    ship movement

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public AnimationCurve shipOscilation;
+    public Vector2 shipScale;
+    public float timeElapsed;
+
+    void Start()
+    {
+        shipOscilation = new AnimationCurve();
+        shipScale = new Vector2(1,1);
+        shipOscilation.AddKey(0, -0.02f);
+        shipOscilation.AddKey(1, 0.02f);
+        shipOscilation.postWrapMode = WrapMode.PingPong;
+        shipOscilation.preWrapMode = WrapMode.PingPong;
+        timeElapsed = 0f;
+    }
+   
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (Input.GetKeyDown("space"))
+        {
+            transform.position = new Vector2(-50,0);
+            transform.localScale = new Vector2(1.5f, 1.5f);
+        } 
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            transform.position = new Vector2(-28,0);
+            transform.localScale = new Vector2(3f, 3f);
+        }
+
+        transform.position = new Vector2(transform.position.x, transform.position.y+shipOscilation.Evaluate(timeElapsed));
+    }
+}

47cbd8ce0843d0a203aa1f39d3bb8094c328fd0d
Marina Blin <m4rina.blin@gmail.com>
2020-02-01 12:21:41 +0100

    :sparkles: Created navigation

---
+++ Assets/Scripts/Helpers/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void GameWin()
+    {
+        SceneManager.LoadScene("GameWin");
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}

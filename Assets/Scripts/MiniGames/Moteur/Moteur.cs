6afc2c7aecbc505404c6777ab2df6e08e3190d76
Valentin Cocaud <v.cocaud@gmail.com>
2020-02-01 01:15:45 +0100

    :sparkles: moteur mini game

---
+++ Assets/Scripts/MiniGames/Moteur/Moteur.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moteur : MonoBehaviour
+{
+    public bool left = true;
+    public int nbDirt;
+    public GameObject[] dirtSprites;
+
+    private void Start()
+    {
+        nbDirt = transform.childCount;
+    }
+
+    private void DeleteOneDirt()
+    {
+        if (transform.childCount > 0)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+            nbDirt--;
+        }
+    }
+
+    void Update()
+    {
+        if (nbDirt == 0 && Input.GetKeyDown(KeyCode.Return))
+        {
+            Destroy(gameObject);
+        }
+        else if (Input.GetKeyDown(left ? KeyCode.RightArrow : KeyCode.LeftArrow))
+        {
+            left = !left;
+            DeleteOneDirt();
+        }
+    }
+}

d96769cf795f05ba07879c030043f07b0a3140c6
Valentin Cocaud <v.cocaud@gmail.com>
2020-02-02 01:28:47 +0100

    :sparkles: ajout du timer

---
+++ Assets/Scripts/WinTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WinTimer : MonoBehaviour
+{
+    public TimeSpan timeSpent = TimeSpan.FromSeconds(0);
+
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSpent += TimeSpan.FromSeconds(Time.deltaTime);
+        text.text = string.Format("{0:mm}’{0:ss}'’{0:ff}", timeSpent);
+    }
+}

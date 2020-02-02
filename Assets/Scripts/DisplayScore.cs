686d954480f88295dcaab151e94c688268eca468
Valentin Cocaud <v.cocaud@gmail.com>
2020-02-02 11:00:20 +0100

    :sparkles: ajout du score au game over

---
+++ Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var score = GameObject.Find("Persisted").GetComponent<Score>().score;
+        GetComponent<Text>().text = string.Format("{0:mm}’{0:ss}'’{0:ff}", score);
+    }
+}
